161f625bc9a107611e3f578dca3e29a5bb862818
Shane <shane8641@live.com>
2016-02-05 09:47:32 -0500

    added requirements to top of file. Commented out.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
+    /*Foo Bar
+    Return the result of "foo," "bar," or "foobar" depending on the input.
+    Return "foo" if the number is dividable by 3
+    Return "bar" if the number is dividable by 5
+    Return "foobar" if the number is dividable by 15
+    Return the same number if no other requirement is met.*/
     public static void main(String[] args) {
+
 
         System.out.println("Hello World!");
     }
